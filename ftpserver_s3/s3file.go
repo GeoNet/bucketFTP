3dc03402ed69e57d7304b5df996e5826b2ce498a
Chris LeBlanc <crleblanc@gmail.com>
2016-10-20 18:24:08 +1300

    Got the upload working without buffering, now using io.Pipe and s3manager.

--- ftpserver_s3/s3file.go
+++ ftpserver_s3/s3file.go
@@ -4,7 +4,6 @@ package main
 // io.Writer, io.Reader, io.Closer, io.Seeker (stubbed out).  S3 manager requires only the io.Reader interface.
 
 import (
-	"bytes"
 	"errors"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -16,14 +15,12 @@ import (
 type S3VirtualFile struct {
 	s3session    *session.Session
 	path         string       // the S3 key
-	writeFile    bool         // only write to S3 if we've seen this flag
-	writeBuffer  bytes.Buffer // a buffer that holds anything we want to write to a key on S3 (the value)
+	s3FileOpen   bool         // only write to S3 if we've seen this flag
 	s3FileIsRead bool
-	readBuffer   *bytes.Buffer
 	reader       *io.PipeReader
 	writer       *io.PipeWriter
 	s3FileOutput *s3.GetObjectOutput
-	svc *s3.S3
+	s3Service    *s3.S3
 }
 
 func NewS3VirtualFile(path string) (*S3VirtualFile, error) {
@@ -36,31 +33,20 @@ func NewS3VirtualFile(path string) (*S3VirtualFile, error) {
 		return nil, err
 	}
 
-	f.svc = s3.New(f.s3session)
+	f.s3Service = s3.New(f.s3session)
+
+	f.reader, f.writer = io.Pipe()
 
 	return f, nil
 }
 
 func (f *S3VirtualFile) Close() error {
 
-	if f.writeFile {
-		uploader := s3manager.NewUploader(f.s3session)
-		upParams := &s3manager.UploadInput{
-			Bucket: &S3_BUCKET_NAME,
-			Key:    &f.path,
-			// TODO would be nice to avoid buffering the file into memory
-			Body: bytes.NewReader(f.writeBuffer.Bytes()),
-		}
-
-		_, err := uploader.Upload(upParams)
-
-		f.writeFile = false
-		return err
-	}
-
 	if f.s3FileIsRead {
 		f.s3FileOutput.Body.Close()
 	}
+
+	f.writer.Close()
 
 	return nil
 }
@@ -75,7 +61,7 @@ func (f *S3VirtualFile) Read(buffer []byte) (int, error) {
 			Key:    &f.path,
 		}
 
-		if f.s3FileOutput, err = f.svc.GetObject(params); err != nil {
+		if f.s3FileOutput, err = f.s3Service.GetObject(params); err != nil {
 			return 0, err
 		}
 
@@ -91,6 +77,29 @@ func (f *S3VirtualFile) Seek(n int64, w int) (int64, error) {
 }
 
 func (f *S3VirtualFile) Write(buffer []byte) (int, error) {
-	f.writeFile = true
-	return f.writeBuffer.Write(buffer)
+	// Using reader and writer pipes to avoid buffering a file in memory
+	if !f.s3FileOpen {
+		f.s3FileOpen = true
+
+		// using a go routine to avoid deadlock waiting on Write
+		go func() {
+
+			defer f.reader.Close()
+
+			uploader := s3manager.NewUploader(f.s3session)
+			upParams := &s3manager.UploadInput{
+				Bucket: &S3_BUCKET_NAME,
+				Key:    &f.path,
+				Body:   f.reader,
+			}
+
+			if _, err := uploader.Upload(upParams); err != nil {
+				log.Println("error uploading file", err)
+				//return
+				// TODO: find a way to abort if the upload fails (hard since we're in a goroutine).  Fatal??
+			}
+		}()
+	}
+
+	return f.writer.Write(buffer)
 }
