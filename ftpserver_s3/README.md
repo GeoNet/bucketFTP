6fe3fe945286a950caaeba9777a06fe338d32d42
Chris LeBlanc <crleblanc@gmail.com>
2016-10-27 13:34:43 +1300

    ChangeDirectory now working (testing manually), added license and updated README.

--- ftpserver_s3/README.md
+++ ftpserver_s3/README.md
@@ -1,33 +1,50 @@
 # FTP Server for S3
 
 This is a Go implementation of an FTP server to allow a user to put or 
-get files from a single S3 bucket.
+get files from a single S3 bucket.  It is free and open source, using 
+the Apache 2.0 license.
 
 ## Security
 
-FTP is an extremely insecure protocol.  We use scientific instruments that
-only support FTP, otherwise would avoid it.  We strongly recommend using
-a private network when using FTP.
+FTP is an extremely insecure protocol.  We use equipment that requires 
+an FTP server for transferring files.  We strongly recommend using a 
+private network when using FTP.
 
 User auth is extremely simple.  It's checking against environment variables.
 This will likely be changed.
 
-TLS is not currently supported.
+TLS is not currently implemented.
+
+File modes on S3 are faked.  Attempting to read or modify a file on S3 with
+insufficient permissions will raise an error.
 
 ## Implementation
 
-This utility uses the ftpserver package from github.com/fclairamb/ftpserver/server 
-with a driver that uses the AWS SDK for Go: github.com/aws/aws-sdk-go/aws.
+This utility uses the [ftpserver] (github.com/fclairamb/ftpserver/server) package. 
+with a custom driver that uses the [AWS SDK for Go] (github.com/aws/aws-sdk-go/aws).
 
 The environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY should
-be supplied (available from the AWS console).
+be supplied (These are available from the AWS console for S3).
+
+## Quickstart
+
+* clone this git repo (or download the archive) from github.com
+* cd to the ftpserver_s3 directory
+* run the command `go build`
+* open the file env.list.  Export each environment variable listed with the appropriate
+value.  Eg: `export FTP_PORT=3000`.  You will need valid AWS credentials.
+* run the ftpserver: `./ftpserver_s3`
+* use an FTP client to connect to your running server, eg on Linux connect to the ftp
+server running on localhost at port 3000: `ftp -p localhost 3000`
 
 ## Important Notes
 
 * Active FTP transfers are not supported, only passive FTP.
-* No buffering or saving to tempfiles is done on the FTP server, this 
+* No buffering or saving to temp files is done on the FTP server, this 
 should let a user upload or download large files (untested).
 * This is a minimal implementation, only the required commands have been
-implemented: get, put, delete, ls.
+implemented: get, put, delete, ls, cd.
 * It was intended to run this from Docker but this server uses random
-port numbers which are poorly supported by docker.
+port numbers which are difficult to support in Docker.
+* All dependencies are vendored using govendor.  Recent versions of Go
+should automatically use these packages.
--- ftpserver_s3/s3driver.go
+++ ftpserver_s3/s3driver.go
@@ -9,6 +9,7 @@ import (
 	"github.com/fclairamb/ftpserver/server"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -19,12 +20,12 @@ type S3Driver struct {
 	s3Service *s3.S3
 }
 
-func (driver *S3Driver) WelcomeUser(cc server.ClientContext) (string, error) {
+func (d *S3Driver) WelcomeUser(cc server.ClientContext) (string, error) {
 	cc.SetDebug(true)
-	return "Welcome to the GeoNet S3 FTP server", nil
+	return "Welcome to the FTP server for S3", nil
 }
 
-func (driver *S3Driver) AuthUser(cc server.ClientContext, user, pass string) (server.ClientHandlingDriver, error) {
+func (d *S3Driver) AuthUser(cc server.ClientContext, user, pass string) (server.ClientHandlingDriver, error) {
 
 	if user != FTP_USER {
 		log.Println("username does not match expected user", user)
@@ -42,41 +43,65 @@ func (driver *S3Driver) AuthUser(cc server.ClientContext, user, pass string) (se
 		return nil, err
 	}
 
-	return driver, nil
+	return d, nil
 }
 
-func (driver *S3Driver) GetTLSConfig() (*tls.Config, error) {
+func (d *S3Driver) GetTLSConfig() (*tls.Config, error) {
 	return nil, errors.New("TLS not implemented")
 }
 
-func (driver *S3Driver) ChangeDirectory(cc server.ClientContext, directory string) error {
-	//if directory == "/debug" {
-	//	cc.SetDebug(!cc.Debug())
-	//	return nil
-	//} else if directory == "/virtual" {
-	//	return nil
-	//}
-	//_, err := os.Stat(driver.baseDir + directory)
-	//return err
-	return errors.New("ChangeDirectory not implemented")
+func (d *S3Driver) ChangeDirectory(cc server.ClientContext, directory string) error {
+	// Query S3 to see if the directory exists.  If it does update the baseDir variable to emulate cd-ing to that
+	// directory
+
+	var err error
+	var dirname string
+
+	// join paths and keep relative to root dir.
+	if dirname, err = filepath.Rel("/", directory); err != nil {
+		return err
+	}
+
+	if dirname == "." {
+		dirname = ""
+	}
+
+	delimiter := "/"
+	params := &s3.ListObjectsV2Input{
+		Bucket:    &S3_BUCKET_NAME,
+		Prefix:    &dirname,
+		Delimiter: &delimiter, // limits the search to directories (they have a trailing slash)
+	}
+
+	var resp *s3.ListObjectsV2Output
+	if resp, err = d.s3Service.ListObjectsV2(params); err != nil {
+		return err
+	}
+
+	if *resp.KeyCount == 0 {
+		return errors.New("No such directory")
+	}
+
+	d.baseDir = dirname
+
+	return nil
 }
 
-func (driver *S3Driver) MakeDirectory(cc server.ClientContext, directory string) error {
+func (d *S3Driver) MakeDirectory(cc server.ClientContext, directory string) error {
 	//return os.Mkdir(driver.baseDir+directory, 0777)
 	return errors.New("MakeDirectory not implemented")
 }
 
-func (driver *S3Driver) ListFiles(cc server.ClientContext) ([]os.FileInfo, error) {
+func (d *S3Driver) ListFiles(cc server.ClientContext) ([]os.FileInfo, error) {
 
 	params := &s3.ListObjectsV2Input{
 		Bucket: &S3_BUCKET_NAME,
-		//Prefix: &driver.baseDir,
-		//Delimiter: keys ending with this string, a bit misleading
+		Prefix: &d.baseDir,
 	}
 
 	var err error
 	var resp *s3.ListObjectsV2Output
-	if resp, err = driver.s3Service.ListObjectsV2(params); err != nil {
+	if resp, err = d.s3Service.ListObjectsV2(params); err != nil {
 		return nil, err
 	}
 
@@ -95,30 +120,30 @@ func (driver *S3Driver) ListFiles(cc server.ClientContext) ([]os.FileInfo, error
 	return files, nil
 }
 
-func (driver *S3Driver) UserLeft(cc server.ClientContext) {
+func (d *S3Driver) UserLeft(cc server.ClientContext) {
 
 }
 
-func (driver *S3Driver) OpenFile(cc server.ClientContext, path string, flag int) (server.FileStream, error) {
+func (d *S3Driver) OpenFile(cc server.ClientContext, path string, flag int) (server.FileStream, error) {
 	// our implementation uses an interface that mimics a file but uploads/downloads to S3
 	var err error
 	var s3file *S3VirtualFile
 
-	if s3file, err = NewS3VirtualFile(path, driver.s3Session, driver.s3Service); err != nil {
+	if s3file, err = NewS3VirtualFile(path, d.s3Session, d.s3Service); err != nil {
 		return nil, err
 	}
 
 	return s3file, nil
 }
 
-func (driver *S3Driver) GetFileInfo(cc server.ClientContext, path string) (os.FileInfo, error) {
+func (d *S3Driver) GetFileInfo(cc server.ClientContext, path string) (os.FileInfo, error) {
 
 	params := &s3.GetObjectInput{
 		Bucket: &S3_BUCKET_NAME,
 		Key:    &path,
 	}
 
-	req, resp := driver.s3Service.GetObjectRequest(params)
+	req, resp := d.s3Service.GetObjectRequest(params)
 
 	if err := req.Send(); err != nil {
 		return nil, err
@@ -136,25 +161,25 @@ func (driver *S3Driver) GetFileInfo(cc server.ClientContext, path string) (os.Fi
 	return f, nil
 }
 
-func (driver *S3Driver) CanAllocate(cc server.ClientContext, size int) (bool, error) {
+func (d *S3Driver) CanAllocate(cc server.ClientContext, size int) (bool, error) {
 	return true, nil
 }
 
-func (driver *S3Driver) ChmodFile(cc server.ClientContext, path string, mode os.FileMode) error {
+func (d *S3Driver) ChmodFile(cc server.ClientContext, path string, mode os.FileMode) error {
 	return errors.New("ChmodFile not implemented")
 }
 
-func (driver *S3Driver) DeleteFile(cc server.ClientContext, path string) error {
+func (d *S3Driver) DeleteFile(cc server.ClientContext, path string) error {
 	params := &s3.DeleteObjectInput{
 		Bucket: &S3_BUCKET_NAME,
 		Key:    &path,
 	}
 
-	_, err := driver.s3Service.DeleteObject(params)
+	_, err := d.s3Service.DeleteObject(params)
 	return err
 }
 
-func (driver *S3Driver) RenameFile(cc server.ClientContext, from, to string) error {
+func (d *S3Driver) RenameFile(cc server.ClientContext, from, to string) error {
 
 	// S3 doesn't have rename (or move) so we need to copy (CopyObject) and delete the old one (DeleteObject).  Put off for now.
 
@@ -172,7 +197,7 @@ func (driver *S3Driver) RenameFile(cc server.ClientContext, from, to string) err
 	return errors.New("RenameFile not implemented")
 }
 
-func (driver *S3Driver) GetSettings() *server.Settings {
+func (d *S3Driver) GetSettings() *server.Settings {
 	config := server.Settings{
 		Host:           "0.0.0.0",
 		Port:           FTP_PORT,
@@ -196,6 +221,9 @@ func NewS3Driver() (*S3Driver, error) {
 	}
 
 	driver.s3Service = s3.New(driver.s3Session)
+
+	// an empty string corresponds to the root of the S3 bucket
+	driver.baseDir = ""
 
 	return driver, nil
 }
