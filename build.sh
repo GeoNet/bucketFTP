b786ddd953d5f87602be3b394ed800d96718148c
Chris LeBlanc <crleblanc@gmail.com>
2016-11-08 11:31:47 +1300

    Change to build.sh to fetch ca-certs and tzinfo inside docker container instead of host

--- build.sh
+++ build.sh
@@ -15,21 +15,18 @@ VERSION='git-'`git rev-parse --short HEAD`
 # The current working dir to use in GOBIN etc e.g., geonet-web
 CWD=${PWD##*/}
 
+mkdir -p ${DOCKER_TMP}/etc/ssl/certs
+mkdir -p ${DOCKER_TMP}/usr/share
+
 # Assemble common resource for ssl and timezones from the build container
-docker run --rm -v "$PWD":"$PWD" ${BUILD_CONTAINER} \
-	apk add --update ca-certificates tzdata; \
-	mkdir -p "$PWD"/${DOCKER_TMP}/etc/ssl/certs; \
-	mkdir -p "$PWD"/${DOCKER_TMP}/usr/share; \
-	cp /etc/ssl/certs/ca-certificates.crt "$PWD"/${DOCKER_TMP}/etc/ssl/certs; \
-	cp -Ra /usr/share/zoneinfo "$PWD"/${DOCKER_TMP}/usr/share
+docker run --rm -v ${PWD}:${PWD} ${BUILD_CONTAINER} \
+    /bin/ash -c "apk add --update ca-certificates tzdata && \
+    cp /etc/ssl/certs/ca-certificates.crt ${PWD}/${DOCKER_TMP}/etc/ssl/certs && \
+    cp -Ra /usr/share/zoneinfo ${PWD}/${DOCKER_TMP}/usr/share"
 
-#echo "nobody:x:65534:65534:Nobody:/:" > ${DOCKER_TMP}/etc/passwd
-
-#for i in "$@"
-#do
 pkgname=${CWD}
 docker run -e "GOBIN=/usr/src/go/src/github.com/GeoNet/${pkgname}/${DOCKER_TMP}" -e GOPATH=/usr/src/go -e "CGO_ENABLED=0" -e "GOOS=linux" -e "BUILD=$BUILD" --rm \
-    -v "$PWD":/usr/src/go/src/github.com/GeoNet/${pkgname} \
+    -v ${PWD}:/usr/src/go/src/github.com/GeoNet/${pkgname} \
     -w /usr/src/go/src/github.com/GeoNet/${pkgname} ${BUILD_CONTAINER} \
     go install -a -ldflags "-X main.Prefix=${pkgname}/${VERSION}" -installsuffix cgo .
 
@@ -38,4 +35,5 @@ cd ${DOCKER_TMP}
 docker build -t bucketftp:latest .
 cd ..
 
-rm -rf ${DOCKER_TMP}
+# Use rm in docker to avoid permissions issues
+docker run -v ${PWD}:${PWD} ${BUILD_CONTAINER} rm -rf ${PWD}/${DOCKER_TMP}
